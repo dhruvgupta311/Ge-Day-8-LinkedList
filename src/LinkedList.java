b82d75a4562859e2abd907d2bc22372f3e81fe1c
Dhruv <guptadhruv1908@gmail.com>
2024-12-10 20:36:13 +0530

    uc8 commit

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -49,6 +49,18 @@ public class LinkedList {
         }
         return false;
     }
+    void insertAfterValue(int value, int data) {
+        Node current = head;
+        while (current != null) {
+            if (current.data == value) {
+                Node newNode = new Node(data);
+                newNode.next = current.next;
+                current.next = newNode;
+                return;  // Inserted after the node with the given value
+            }
+            current = current.next;
+        }
+    }
 
     void insertAtPosition(int position, int data) {
         Node newNode = new Node(data);
